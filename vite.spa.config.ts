3f5e220537cc1a33e7bb793236b42911c95aee15
PadhleYrr <moliage490@gmail.com>
2026-06-17 18:37:09 +0530

    fix: vite.spa.config.ts

--- vite.spa.config.ts
+++ vite.spa.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ routesDirectory: "./src/routes", generatedRouteTree: "./src/routeTree.gen.ts" }),
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+    }),
     react(),
     tailwindcss(),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     outDir: "dist-spa",
     emptyOutDir: true,
+    rollupOptions: {
+      input: "index.spa.html",
+    },
   },
 });
